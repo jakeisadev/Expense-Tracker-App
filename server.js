1d223d92bcaaba34acc9a604aa1a80fa322f5a35
jakeisadev <starowicz@protonmail.com>
2022-10-08 12:42:00 -0400

    package.json update

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ var cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+const path = require('path')
 const userRoute = require('./routes/usersRoute')
 const transactionsRoute = require('./routes/transactionsRoute')
 app.use('/api/users', userRoute)
 app.use('/api/transactions', transactionsRoute)
 
 
-const port = 5000
+const port = process.env.POT || 5000
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`Node JS server started at ${port}!`))
